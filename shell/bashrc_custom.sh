5b76205b1f2e3dab30b29f38c40fb9d4cb1a5d12
devcxl <64475363+devcxl@users.noreply.github.com>
2022-12-31 22:26:27 +0800

    update command about

--- shell/bashrc_custom.sh
+++ shell/bashrc_custom.sh
@@ -6,6 +6,7 @@ df -h | awk '$NF=="/"{printf "磁盘使用: %d/%dGB (%s)\n", $3,$2,$5}'
 alias uuid='cat /proc/sys/kernel/random/uuid | sed "s/-//g"'
 
 # deps scrcpy
+# https://github.com/Genymobile/scrcpy/wiki/README.zh-Hans
 alias scrcpy='scrcpy --push-target /storage/emulated/0/Download/ -m 1080 -b 4M --hid-keyboard --turn-screen-off'
 
 # deps git
