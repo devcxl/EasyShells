2697e6d4cddb3e4d8f2bf537c3612afea599b2d8
devcxl <64475363+devcxl@users.noreply.github.com>
2023-04-24 23:21:18 +0800

    update: v2rayinstall

--- src/v2rayinstall/help.sh
+++ src/v2rayinstall/help.sh
@@ -5,16 +5,18 @@ cat <<EOF
 -h | --help | -help | help        使用帮助
 relay                             为中转服务器安装VMESS+WS类型的代理服务
     -p    | --in-port             中转服务器的端口      default: random_port
-    -uid  | --in-uuid             中转服务器的UUID账户  default: random_uuid
+    -i    | --in-uuid             中转服务器的UUID账户  default: random_uuid
     -path | --in-websocket-path   中转服务器的WebSocket路径 default: 'openssl rand -hex 16'
     -D    | --out-domain          被中转服务器的域名
     -P    | --out-port            被中转服务器的端口
-    -UID  | --out-uuid            被中转服务器中可用的UUID
+    -I    | --out-uuid            被中转服务器中可用的UUID
     -PATH | --out-websocket-path  被中转服务器的WebSocket路径
 
 direct                            为直连服务器安装VMESS+WS+TLS类型的代理服务
-    -p | --port                   直连服务器的端口
-    -i | --uuid                   直连服务器的UUID default: random_uuid
-    -d | --domain                 直连服务器可用域名 tips: 要求域名解析已经配置
+    -p    | --in-port             直连服务器的端口
+    -i    | --in-uuid             直连服务器的UUID default: random_uuid
+    -path | --in-websocket-path   中转服务器的WebSocket路径 default: 'openssl rand -hex 16'
+    -d    | --domain              直连服务器可用域名 tips: 要求域名解析已经配置
+    -t    | --tls                 为域名配置tls
 EOF
 }
--- src/v2rayinstall/relay.sh
+++ src/v2rayinstall/relay.sh
@@ -3,5 +3,57 @@ relay(){
     bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
     bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh)
     
+    # download config template
+    curl -o $V2RAY_CONFIG_PATH -L https://raw.githubusercontent.com/devcxl/EasyShells/master/src/v2rayinstall/template/relayConfig.json.template
+
+    # configure config
+    if [ -z "$IN_PORT" ]; then
+        random_port
+        IN_PORT=$random_port
+    fi
+    sed -i "s/{IN_PORT}/$IN_PORT/g" $V2RAY_CONFIG_PATH
+
+    if [ -z "$IN_UUID" ]; then
+        IN_UUID=$(cat /proc/sys/kernel/random/uuid)
+    fi
+    sed -i "s/{IN_UUID}/$IN_UUID/g" $V2RAY_CONFIG_PATH
+
+    if [ -z "$IN_WS_PATH" ]; then
+        IN_WS_PATH=/$(openssl rand -hex 16)
+    fi
+    sed -i "s/{IN_WS_PATH}/$IN_WS_PATH/g" $V2RAY_CONFIG_PATH
+
+
+    if [ -z "$OUT_DOMAIN" ]; then
+        error "\$OUT_DOMAIN not be null."
+    fi
+    sed -i "s/{OUT_DOMAIN}/$OUT_DOMAIN/g" $V2RAY_CONFIG_PATH
+
+
+    if [ -z "$OUT_PORT" ]; then
+        error "\$OUT_PORT not be null."
+    fi
+    sed -i "s/{OUT_PORT}/$OUT_PORT/g" $V2RAY_CONFIG_PATH
+
+
+    if [ -z "$OUT_UUID" ]; then
+        error "\$OUT_UUID not be null."
+    fi
+    sed -i "s/{OUT_UUID}/$OUT_UUID/g" $V2RAY_CONFIG_PATH
+
+
+    if [ -z "$OUT_WS_PATH" ]; then
+        error "\$OUT_WS_PATH not be null."
+    fi
+    sed -i "s/{OUT_WS_PATH}/$OUT_WS_PATH/g" $V2RAY_CONFIG_PATH
+
+    # warn: v2ray version must > 5
+    if  v2ray test -c $V2RAY_CONFIG_PATH ;then
+        info 'config.json verify successful.'
+        systemctl enable --now v2ray
+    fi
+
+    systemctl enable --now nginx
+
 
 }
