a362efd9fc87b3e2469db9945734b107a728a0f3
devcxl <64475363+devcxl@users.noreply.github.com>
2023-04-24 22:00:17 +0800

    add: v2rayinstall doing

---
+++ src/v2rayinstall/param_parse.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+param_parse(){
+    while [[ "$#" -gt '0' ]]; do
+        case $1 in
+        'relay')
+            RELAY='1'
+            ;;
+        'direct')
+            DIRECT='1'
+            ;;
+        '-h' | '--help' | '-help' | 'help')
+            HELP='1'
+            ;;
+        '-d' | '--dir')
+            DIR="${2:?error: path error.}"
+            shift
+            ;;
+        '-c' | '--client-name')
+            CLIENT_NAME="${2:?error: client name error.}"
+            shift
+            ;;
+        '-s' | '--server-name')
+            SERVER_NAME="${2:?error: client name error.}"
+            shift
+            ;;
+        '-p' | '--passphrase')
+            PASSPHRASE="${2}"
+            shift
+            ;;
+        '-C' | '--copy-id')
+            COPY_FLAG=1
+            COPY_2_ADDRESS="${2}"
+            shift
+            ;;
+        *)
+            error "$0: unknown option $1"
+            ;;
+        esac
+        shift
+    done
+}
---
+++ src/v2rayinstall/relay.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+relay(){
+    bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+    bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh)
+    
+
+}
