be640936a21bc3b04e8041f00c217d3d67256889
devcxl <64475363+devcxl@users.noreply.github.com>
2023-07-13 01:39:17 +0800

    update: bashrc_custom.sh

--- src/bashrc_custom.sh
+++ src/bashrc_custom.sh
@@ -1,9 +1,11 @@
 # Linux Custom Terminal
 echo -e "用户：${USER} "
-date "+%A %d %B %Y, %T"
+date "+%Y年%B%d日%A %T"
 free -h | awk 'NR==2{printf "内存使用: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
 df -h | awk '$NF=="/"{printf "磁盘使用: %d/%dGB (%s)\n", $3,$2,$5}'
-alias uuid='cat /proc/sys/kernel/random/uuid | sed "s/-//g"'
+
+alias uuid='cat /proc/sys/kernel/random/uuid'
+alias uuid-='cat /proc/sys/kernel/random/uuid | sed "s/-//g"'
 
 # deps scrcpy
 # https://github.com/Genymobile/scrcpy/wiki/README.zh-Hans
@@ -12,6 +14,8 @@ alias scrcpy='scrcpy --push-target /storage/emulated/0/Download/ -m 1080 -b 4M -
 # deps git
 alias git-proxy='git config --global http.proxy socks://127.0.0.1:1089'
 alias git-unproxy='git config --global --unset https.proxy'
+alias git-user='git config user.name'
+alias git-mail='git config user.email'
 
 # deps docker
 alias redis-server='docker run --rm -it --network host redis:5-alpine redis-server'
@@ -23,3 +27,7 @@ alias code='$HOME/apps/VSCode-linux-x64/bin/code'
 # Python export & install deps
 alias py-export='pip freeze > requirements.txt'
 alias py-install='pip install -r requirements.txt'
+
+alias ll='ls -l'
+alias la='ls -a'
+alias lla='ls -l -a'
